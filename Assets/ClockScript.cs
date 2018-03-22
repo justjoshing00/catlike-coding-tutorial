c4333e0747e847ab75ee2940414fab69c4f7d440
josh oji <selmlocke@gmail.com>
2018-03-22 16:23:17 -0400

    first change

---
+++ Assets/ClockScript.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using UnityEngine;
+
+public class ClockScript : MonoBehaviour{
+    const float degreesPerHour = 30f;
+    const float degreesPerMinute = 6f;
+    const float degreesPerSecond = 6f;
+
+    public Transform hoursTransform;
+    public Transform minutesTransform;
+    public Transform secondsTransform;
+
+    public bool continuous;
+
+    private void UpdateContinuous()
+    {
+        //Debug.Log(DateTime.Now);
+        TimeSpan time = DateTime.Now.TimeOfDay;
+
+        hoursTransform.localRotation =
+            Quaternion.Euler(0f, (float)time.TotalHours * degreesPerHour, 0f);
+        minutesTransform.localRotation =
+            Quaternion.Euler(0f, (float)time.TotalMinutes * degreesPerMinute, 0f);
+        secondsTransform.localRotation =
+            Quaternion.Euler(0f, (float)time.TotalSeconds * degreesPerSecond, 0f);
+    }
+    private void UpdateDiscrete()
+    {
+        //Debug.Log(DateTime.Now);
+        DateTime time = DateTime.Now;
+
+        hoursTransform.localRotation =
+            Quaternion.Euler(0f, time.Hour * degreesPerHour, 0f);
+        minutesTransform.localRotation =
+            Quaternion.Euler(0f, time.Minute * degreesPerMinute, 0f);
+        secondsTransform.localRotation =
+            Quaternion.Euler(0f, time.Second * degreesPerSecond, 0f);
+    }
+
+            void Update()
+            {
+            if (continuous)
+                {
+                    UpdateContinuous();
+                }
+            else
+                {
+                    UpdateDiscrete();
+                }
+            }
+
+
+
+    }
